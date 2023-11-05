7247b881754204e2c2336f5e78638c0aa8bf2536
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-11-05 19:20:46 +0330

    Setting up tailwindcss

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import Index from "./pages/Index/Index";
 import CourseInfo from "./pages/CourseInfo/CourseInfo";
 import ArticleInfo from "./pages/ArticleInfo/ArticleInfo";
 import Category from "./pages/Category/Category";
-import React from "react";
+
 const routes = [
   { path: "/", element: <Index /> },
   { path: "/course-info/:courseName", element: <CourseInfo /> },
