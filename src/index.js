7a8679e2feed2e4338cbab144f54664c6ef9fb8b
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-11-05 20:40:47 +0330

    changed css styles to tailwind

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import "./styles/fonts.css";
-import "./styles/reset.css";
-import "./styles/defaults.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
