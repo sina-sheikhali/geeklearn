fc5e611c879b8e56ca392cd2cbcb65a6803eac85
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-11-15 11:07:25 +0330

    Customize the slider

--- src/Components/LatestCourses/LatestCourses.js
+++ src/Components/LatestCourses/LatestCourses.js
@@ -4,15 +4,34 @@ import { Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import SectionHeader from "../SectionHeader/SectionHeader";
 import CourseBox from "../CourseBox/CourseBox";
+
 export default function LatestCourses() {
   return (
     <div className="container max-w-[1280px] mt-20 p-5">
       <SectionHeader />
-      <div className="mt-5 p-2.5 flex">
+      <div className="mt-5 p-2.5 flex swiper-container">
         <Swiper
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+            },
+
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+            1150: {
+              slidesPerView: 3,
+            },
+          }}
           spaceBetween={20}
-          slidesPerView={3}
           navigation={true}
+          loop={true}
           modules={[Navigation]}
           className="mySwiper"
         >
