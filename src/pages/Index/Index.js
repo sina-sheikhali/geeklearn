84388bead5556eb69d94a4ef9f28b55d231ccbec
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-11-15 17:17:32 +0330

    import components to Index page

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -2,11 +2,19 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 import "./Index.css";
 import LatestCourses from "../../Components/LatestCourses/LatestCourses";
+import AboutUs from "../../Components/AboutUs/AboutUs";
+import LastArticle from "../../Components/LastArticle/LastArticle";
+import Footer from "../../Components/Footer/Footer";
 export default function Index() {
   return (
     <div>
       <Header />
       <LatestCourses />
+      <LatestCourses />
+      <AboutUs />
+      <LatestCourses />
+      <LastArticle />
+      <Footer />
     </div>
   );
 }
