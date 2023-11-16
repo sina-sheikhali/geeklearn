7f77f3c865339864c314317ee825bce00ea8aee8
sina-sh0734 <sinasheikhali.work@gmail.com>
2023-11-16 17:40:18 +0330

    Developed FreeCourses and SuggestedCourses components

---
+++ src/Components/SuggestedCourses/SuggestedCourses.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import SectionHeader from "../SectionHeader/SectionHeader";
+import CourseBox from "../CourseBox/CourseBox";
+
+export default function SuggestedCourses() {
+  return (
+    <div className="container max-w-[1280px] my-20 p-5">
+      <SectionHeader
+        title={"دوره های پیشنهادی"}
+        desc={"گلچینی برای شما ..."}
+        btnTitle={"همه دوره ها"}
+      />
+      <div className="mt-5 p-2.5 flex">
+        <Swiper
+          breakpoints={{
+            320: {
+              slidesPerView: 1,
+            },
+
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+            1150: {
+              slidesPerView: 3,
+            },
+          }}
+          spaceBetween={20}
+          navigation={true}
+          loop={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <CourseBox />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseBox />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseBox />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseBox />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseBox />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CourseBox />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+}
